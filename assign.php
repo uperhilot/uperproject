cded642c3d778b36ad546e35b28d58a93088d3f0
shang <anacarmelapagtama@gmail.com>
2024-09-04 08:29:57 -0800

    First Commit

---
+++ assign.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h3><br>assign:</br></h3";
+echo "<p> 1.Fullname</p>";
+echo "<p> 2.Address</p>" ;
+echo "<p> 3.Contact</p>";
+echo "<p> 4.Hobbies</p>";
+echo "<p>5.Social Media</p>";
+
+?>
---
+++ samplee.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class= "container">
+        <form action="samplee.php"method="post">
+        <label for= "fisrtname">fisrtname:</label>
+        <input type="text" id="firstname" name="firstname"placeholder="Enter Firstname">
+
+
+         <label for= "fisrtname">fisrtname:</label>
+         <input type="text" id="firstname" name="firstname"placeholder="Enter Firstname">
+         
+         <label for="Pet peeve">Pet peeve:</label>
+         <select>
+         <optioin value="none">None</option>
+         <option value="Vea">Vea</option>
+         <option value="Rich Quint">Rich Quint</option>
+         <option vakue="Mark">Mark</option>
+        </select>
+
+        </form>
+
+
+    </div>
+</body>
+</html>
