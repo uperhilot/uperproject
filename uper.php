d3ed027c4789f312f94697792c0a677f1c9f44fc
shang <anacarmelapagtama@gmail.com>
2024-09-05 20:54:40 -0800

    first commit

---
+++ uper.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php 
+    echo "<h2> My Personal Information </h2>";
+    $Fullname="Uperichlie A. Hilot";
+    $Address="P-3,Bangko Panaon,Misamis Occidental";
+    $Contact="09535911461";
+    $Hobbies="Listening music";
+    $Social_media="Facebook,Instagram,Tiktok ,etc.";
+    
+    
+    echo "<strong>My name is</strong>: $Fullname <br>";
+    echo "<strong>I live in</strong>: $Address<br>";
+    echo "<strong>Contact</strong>: $Contact <br>";
+    echo "<strong>I love to</strong>: $Hobbies <br>";
+    echo "<strong>My social media account is</strong>:$Social_media <br>";
+
+    ?>
+    </div>
+</body>
+</html>
